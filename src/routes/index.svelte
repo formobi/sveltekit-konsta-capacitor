bfecfff29d75a253019e202934812cf17528a1d2
Formobi <imtiyaj@formobi.in>
2022-08-17 06:31:53 +0530

    why on:click directive not working?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,21 @@
 <script>
-	/* App.svelte */
-	import { Block, Button } from 'konsta/svelte';
+  /* App.svelte */
+  import { Block, Button } from 'konsta/svelte';
+  let count = 0;
+  function incrementCount() {
+    count += 1;
+  }
 </script>
 
 <Block>
-	<p>This is block with text</p>
+  <p>This is block with text</p>
 </Block>
 <Block class="space-y-4">
-	<p>Here comes the button</p>
-	<Button>Action</Button>
+  <p>'onClick={incrementCount} works'</p>
+  <Button onClick={incrementCount}>Action {count}</Button>
+</Block>
+
+<Block class="space-y-4">
+  <p>'on:click={incrementCount} does not work'</p>
+  <Button on:click={incrementCount}>Action {count}</Button>
 </Block>
